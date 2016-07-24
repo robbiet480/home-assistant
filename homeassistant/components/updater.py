9ee355f93c47580cd5a387db4a7f7e3393b6fccd
Alex Harvey <alex@pixelfactor.ca>
2016-07-24 15:36:24 -0700

    first pass using a test url for reporting the current version

--- homeassistant/components/updater.py
+++ homeassistant/components/updater.py
@@ -5,17 +5,37 @@ For more details about this platform, please refer to the documentation at
 at https://home-assistant.io/components/updater/
 """
 import logging
-
+import os
 import requests
+import uuid
+import json
 
 from homeassistant.const import __version__ as CURRENT_VERSION
 from homeassistant.const import ATTR_FRIENDLY_NAME
 from homeassistant.helpers import event
 
 _LOGGER = logging.getLogger(__name__)
-PYPI_URL = 'https://pypi.python.org/pypi/homeassistant/json'
+UPDATER_URL = 'http://requestb.in/1a39v3a1'
 DOMAIN = 'updater'
 ENTITY_ID = 'updater.updater'
+
+UPDATER_UUID_FILE = "uuid.conf"
+
+def _load_uuid(hass, filename=UPDATER_UUID_FILE):
+    """Load UUID from a file, if it exist if not create it"""
+    path = hass.config.path(filename)
+
+    if not os.path.isfile(path):
+        # file not found create it
+        with open(path, 'w') as uuidfile:
+            uuidfile.write(json.dumps({"uuid":uuid.uuid4().hex}))
+            uuidfile.close()
+
+    try:
+        with open(path) as uuidfile:
+            return uuid.UUID(json.loads(uuidfile.read())['uuid'], version = 4)
+    except (ValueError, AttributeError):
+        return None
 
 
 def setup(hass, config):
@@ -42,17 +62,24 @@ def setup(hass, config):
 
 
 def get_newest_version():
-    """Get the newest Home Assistant version from PyPI."""
+    """Get the newest Home Assistant version."""
+    huuid = _load_uuid(hass)
     try:
-        req = requests.get(PYPI_URL)
+        req = requests.post(
+                    UPDATER_URL,
+                    data=json.dumps({
+                                "uuid":huuid.hex,
+                                "version":CURRENT_VERSION
+                            })
+                    )
 
-        return req.json()['info']['version']
+        return req.json()['version']
     except requests.RequestException:
-        _LOGGER.exception('Could not contact PyPI to check for updates')
+        _LOGGER.exception('Could not contact HASS Update to check for updates')
         return None
     except ValueError:
-        _LOGGER.exception('Received invalid response from PyPI')
+        _LOGGER.exception('Received invalid response from HASS Update')
         return None
     except KeyError:
-        _LOGGER.exception('Response from PyPI did not include version')
+        _LOGGER.exception('Response from HASS Update did not include version')
         return None
