79af5ef3c04ecc467e6213a270729175fdac2d84
Robbie Trencheny <me@robbiet.us>
2016-07-30 19:02:09 -0700

    Add system level information to updater remote call

--- homeassistant/components/updater.py
+++ homeassistant/components/updater.py
@@ -6,15 +6,16 @@ at https://home-assistant.io/components/updater/
 """
 import logging
 import os
-import requests
+import platform
 import uuid
 import json
+from distutils.version import StrictVersion
+import requests
 import homeassistant.util.dt as dt_util
 
 from homeassistant.const import __version__ as CURRENT_VERSION
 from homeassistant.const import ATTR_FRIENDLY_NAME
 from homeassistant.helpers import event
-from distutils.version import StrictVersion
 
 _LOGGER = logging.getLogger(__name__)
 UPDATER_URL = 'https://1cnlq5djza.execute-api.us-west-2.amazonaws.com/prod'
@@ -25,7 +26,7 @@ UPDATER_UUID_FILE = ".uuid"
 
 
 def _load_uuid(hass, filename=UPDATER_UUID_FILE):
-    """Load UUID from a file, if it exist if not create it"""
+    """Load UUID from a file, if it exist if not create it."""
     path = hass.config.path(filename)
 
     if not os.path.isfile(path):
@@ -57,10 +58,8 @@ def setup(hass, config):
         if newest is not None:
             if StrictVersion(newest) > StrictVersion(CURRENT_VERSION):
                 hass.states.set(
-                    ENTITY_ID, newest, {
-                                        ATTR_FRIENDLY_NAME: 'Update Available',
-                                        ATTR_RELEASE_NOTES: releasenotes
-                                        }
+                    ENTITY_ID, newest, {ATTR_FRIENDLY_NAME: 'Update Available',
+                                        ATTR_RELEASE_NOTES: releasenotes}
                 )
 
     event.track_time_change(hass, check_newest_version,
@@ -73,15 +72,26 @@ def setup(hass, config):
 
 def get_newest_version(huuid):
     """Get the newest Home Assistant version."""
+    info_object = {"uuid": huuid, "version": CURRENT_VERSION,
+                   "timezone": dt_util.DEFAULT_TIME_ZONE.zone,
+                   "os_name": platform.system(), "arch": platform.machine(),
+                   "python_version": platform.python_version()}
+
+    if platform.system() == 'Windows':
+        info_object['os_version'] = platform.win32_ver()[0]
+    elif platform.system() == 'Darwin':
+        info_object['os_version'] = platform.mac_ver()[0]
+    elif platform.system() == 'Linux':
+        # This isn't the best, since 1) deprecated in 3.5 and expected to be
+        # removed in 3.7 and 2) http://stackoverflow.com/a/20155573/486182.
+        # Alternative is https://github.com/nir0s/distro.
+        # pylint: disable=deprecated-method
+        linux_dist = platform.linux_distribution()
+        info_object['distribution'] = linux_dist[0]
+        info_object['os_version'] = linux_dist[1]
+
     try:
-        req = requests.post(
-                    UPDATER_URL,
-                    json={
-                            "uuid": huuid,
-                            "version": CURRENT_VERSION,
-                            "timezone": dt_util.DEFAULT_TIME_ZONE.zone,
-                        }
-                    )
+        req = requests.post(UPDATER_URL, json=info_object)
         return (req.json()['version'], req.json()['release-notes'])
     except requests.RequestException:
         _LOGGER.exception('Could not contact HASS Update to check for updates')
