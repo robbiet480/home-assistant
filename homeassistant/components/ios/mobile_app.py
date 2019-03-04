17e72795d9df6d5d266b04e52aec27115559a0c7
Robbie Trencheny <me@robbiet.us>
2019-03-04 05:16:22 -0800

    Add get_push_config webhook type to iOS

--- homeassistant/components/ios/mobile_app.py
+++ homeassistant/components/ios/mobile_app.py
@@ -4,15 +4,24 @@ from aiohttp.web import json_response
 
 from homeassistant.helpers import template
 
+from . import CONF_PUSH, DOMAIN
+
 _LOGGER = logging.getLogger(__name__)
+
+WEBHOOK_TYPE_GET_PUSH_CONFIG = 'get_push_config'
 WEBHOOK_TYPE_RENDER_COMPLICATIONS = 'render_complications'
 
-WEBHOOK_TYPES = [WEBHOOK_TYPE_RENDER_COMPLICATIONS]
+WEBHOOK_TYPES = [WEBHOOK_TYPE_GET_PUSH_CONFIG,
+                 WEBHOOK_TYPE_RENDER_COMPLICATIONS]
 
 
 async def async_handle_webhook_message(hass, device, webhook_type, data):
     """Return a webhook response for the given arguments."""
-    _LOGGER.info("iOS: received msg %s %s %s", webhook_type, device, data)
+    _LOGGER.debug("iOS: received webhook %s %s %s", webhook_type, device,
+                  data)
+
+    if webhook_type == WEBHOOK_TYPE_GET_PUSH_CONFIG:
+        return json_response(hass.data[DOMAIN][CONF_PUSH])
 
     if webhook_type == WEBHOOK_TYPE_RENDER_COMPLICATIONS:
         resp = {}
